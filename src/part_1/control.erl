301d4e8dadaa2c217a58c175569d940f898c6346
Corentin Detry <corentin.detry@gmail.com>
2024-11-26 14:14:21 +0100

    Refactor DHT control and node management; add key handling and node info retrieval
    Co-authored-by: jack-chan-123 <wing.chan@student.uclouvain.be>

--- src/part_1/control.erl
+++ src/part_1/control.erl
@@ -1,5 +1,6 @@
 -module(control).
--import(dht, [start/1, put/3, get/2, add_key/2]).
+-import(dht, [start/1, get_node_info/1, add_key/2, calculate_hash/1, get_node_info/1]).
+-import(lists, [nth/2, map/2]).
 -export([start/0, init/0]).
 
 start() ->
@@ -9,29 +10,52 @@ init() ->
     % Read keys from keys.csv
     {ok, KeysData} = file:read_file("keys.csv"),
     Keys = parse_keys(KeysData),
-    
+
     % Read queries from key_queries.csv
-    {ok, QueriesData} = file:read_file("key_queries.csv"),
-    Queries = parse_keys(QueriesData),
-    
+    % {ok, QueriesData} = file:read_file("key_queries.csv"),
+    % Queries = parse_keys(QueriesData),
+
+    % test: hash keys and print it out
+    % HeadKeys = [nth(X, Keys) || X <- lists:seq(1, 10)],
+    % io:format("Keys: ~p~n", [HeadKeys]),
+    % map(fun(X) -> Hash = calculate_hash(X), io:format("hash: ~p -> ~p ~n", [X, Hash])  end, HeadKeys),
+
     % Start initial DHT node
-    {ok, Nodes} = dht:start(10), % Start with 10 nodes
+
+    % Start with 10 nodes
+    {ok, Nodes} = dht:start(10),
 
     % Add keys to DHT
-    lists:foreach(fun(Key) -> add_key(lists:nth(1, Nodes), Key) end, Keys),
-     
-    % Process queries
-    process_queries(Queries, Nodes).
+    add_keys_to_dht(Keys, Nodes),
+
+    % figure out how many keys are stored in each node
+    Data = map(
+        fun(Node) ->
+            {NodeId, _, Succ, Pred, KeyRecvd} = get_node_info(Node),
+            io:format("Node ~p: Succ ~p, Pred ~p~n", [NodeId, Succ, Pred]),
+            maps:size(KeyRecvd)
+        end,
+        Nodes
+    ),
+    io:format("Keys stored in each node: ~p~n", [Data]).
+
+% Process queries
+% process_queries(Queries, Nodes).
+
+% add keys to DHT
+add_keys_to_dht(Keys, Nodes) ->
+    lists:foreach(fun(Key) -> dht:add_key(Nodes, Key) end, Keys).
 
 parse_keys(Data) ->
     Lines = string:tokens(binary_to_list(Data), "\n\r"),
     [list_to_integer(Line) || Line <- Lines].
 
-process_queries([], _) -> ok;
-process_queries([Query|Rest], [FirstNode|_]=Nodes) ->
-    FirstNode ! {get, Query, self()},
-    receive
-        {Query, Value} ->
-            io:format("Query: ~p -> value: ~p~n", [Query, Value])
-    end,
-    process_queries(Rest, Nodes).
+% process_queries([], _) ->
+%     ok;
+% process_queries([Query | Rest], [FirstNode | _] = Nodes) ->
+%     FirstNode ! {get, Query, self()},
+%     receive
+%         {Query, Value} ->
+%             io:format("Query: ~p -> value: ~p~n", [Query, Value])
+%     end,
+%     process_queries(Rest, Nodes).
--- src/part_1/dht.erl
+++ src/part_1/dht.erl
@@ -1,6 +1,7 @@
 -module(dht).
--export([start/1, init_node/2, put/3, get/2, get_node_info/1, add_key/2]).
+-export([start/1, init_node/2, get_node_info/1, add_key/2, calculate_hash/1]).
 -import(crypto, [hash/2]).
+
 -define(M, 16).
 -define(Max_Key, math:pow(2, ?M) - 1).
 
@@ -9,15 +10,17 @@ start(N) ->
     NodeIds = lists:seq(0, N - 1),
 
     % initialize nodes, Nodes is the list of node Pids
-    Nodes = lists:map(fun(Id) -> spawn(?MODULE, init_node, [Id, NodeIds]) end, NodeIds),
-    % link nodes to main process, so they die when main process dies
-    lists:foreach(fun(Node) -> link(Node) end, Nodes),
+    Nodes = lists:map(
+        fun(Id) ->
+            spawn(?MODULE, init_node, [Id, NodeIds])
+        end,
+        NodeIds
+    ),
     {ok, Nodes}.
 
 % Initializes a node with an identifier and the ring of nodes
 init_node(Id, NodeIds) ->
-    % Identifier = calculate_hash(Id),
-    Identifier = Id,
+    Identifier = calculate_hash(Id),
     Successor = find_successor(Id, NodeIds),
     Predecessor = find_predecessor(Id, NodeIds),
     Keys = #{},
@@ -28,45 +31,46 @@ loop(Id, Identifier, Successor, Predecessor, Keys) ->
     receive
         {add_key, Key} ->
             % add the key to the map
-            HashedKey = calculate_hash(Key),
-            NewKeys = maps:put(HashedKey, undefined, Keys),
+            % key is already hashed
+            NewKeys = maps:put(Key, undefined, Keys),
+            % io:format("Key ~p added to Node ~p~n", [Key, Id]),
             loop(Id, Identifier, Successor, Predecessor, NewKeys);
-        {put, Key, Value} ->
-            HashedKey = calculate_hash(Key),
-            % if the key is already in the map, update the value, otherwise add the key-value pair
-            NewKeys = maps:put(HashedKey, Value, Keys),
-            loop(Id, Identifier, Successor, Predecessor, NewKeys);
-        {get, Key, Caller} ->
-            HashedKey = calculate_hash(Key),
-            % if the key is in the map, return the value, otherwise return undefined
-            case maps:find(HashedKey, Keys) of
-                {ok, Value} -> Caller ! {Key, Value};
-                error -> Caller ! {Key, undefined}
-            end,
-            loop(Id, Identifier, Successor, Predecessor, Keys);
         {get_info, Caller} ->
+            % return the node information
             Caller ! {Id, Identifier, Successor, Predecessor, Keys},
             loop(Id, Identifier, Successor, Predecessor, Keys);
         stop ->
             ok
     end.
 
+% Add a key to the DHT
+% Nodes is the list of node Pids
+% Key is the key to add (not hashed)
+add_key(Nodes, Key) ->
+    % Nodes is the list of node Pids
+    HashedKey = calculate_hash(Key),
+    {_, ResponsibleNode} = find_responsible_node(HashedKey, Nodes),
+    ResponsibleNode ! {add_key, HashedKey}.
 
-add_key(Node, Key) ->
-    Node ! {add_key, Key}.
-
-% Add a key-value pair to the DHT
-put(Node, Key, Value) ->
-    Node ! {put, Key, Value}.
-
-% Get the value of a key from the DHT
-get(Node, Key) ->
-    Node ! {get, Key, self()},
-    receive
-        {Key, Value} -> Value
-    after 5000 -> % timeout
-        undefined
-    end.
+% Find the node responsible for a hashed key
+% Nodes is the list of node Pids
+% HashedKey is the hashed key
+find_responsible_node(HashedKey, Nodes) ->
+    % get the node information for each node
+    NodeInfoList = lists:map(
+        fun(Node) ->
+            {_, NodeId, _, _, _} = get_node_info(Node),
+            {NodeId, Node}
+        end,
+        Nodes
+    ),
+    % filter the list of nodes to find the first node that has an Id greater than the hashed key, otherwise wrap around
+    ResponsibleNode =
+        case lists:filter(fun({NodeId, _}) -> HashedKey =< NodeId end, NodeInfoList) of
+            [] -> hd(NodeInfoList);
+            [{NodeId, Node} | _] -> {NodeId, Node}
+        end,
+    ResponsibleNode.
 
 % Request node information
 get_node_info(Node) ->
@@ -77,10 +81,14 @@ get_node_info(Node) ->
 
 % Utility: Find successor node
 find_successor(Id, NodeIds) ->
-    case [X || X <- NodeIds, X > Id] of % basically, make a list of Xs that come from the NodeIds list, where X is greater than Id
-        [] -> hd(NodeIds); % if the list is empty, return the head of the NodeIds list 
-        [Successor | _] -> Successor % otherwise, return the first element of the list  
-    end. % so what this does is that it finds the next node in the ring that is greater than the current node, if there is none (i.e., last node), it returns the first node in the ring
+    % basically, make a list of Xs that come from the NodeIds list, where X is greater than Id
+    case [X || X <- NodeIds, X > Id] of
+        % if the list is empty, return the head of the NodeIds list
+        [] -> hd(NodeIds);
+        % otherwise, return the first element of the list
+        [Successor | _] -> Successor
+        % so what this does is that it finds the next node in the ring that is greater than the current node, if there is none (i.e., last node), it returns the first node in the ring
+    end.
 
 % Utility: Find predecessor node
 find_predecessor(Id, NodeIds) ->
@@ -92,5 +100,7 @@ find_predecessor(Id, NodeIds) ->
 
 % Utility: Calculate SHA-1 hash of an identifier
 calculate_hash(Key) ->
-    String = io_lib:format("~p", [Key]),
-	crypto:bytes_to_integer(crypto:hash(sha512, String)).
+    IntKey = integer_to_list(Key),
+    crypto:bytes_to_integer(crypto:hash(sha, IntKey)).
+
+    % erlang:phash2(IntKey, 1 bsl ?M).
